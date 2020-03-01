af752f57bdbc34a3a120c03e97bc0f61fc6b2c0e
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2020-03-01 16:45:56 +0000

    added htmlize for my darktooth-themed setup and some neat styling

--- gulpfile.js
+++ gulpfile.js
@@ -10,46 +10,46 @@ const $replace = require('gulp-replace');
 const del = require('del');
 const server = require('browser-sync').create();
 
-$.task('build', $.series(clean, $.parallel(pages, styles, misc)));
-$.task('default', $.series('build', $.parallel(serve, watch)));
-$.task('publish', publish);
+$.task('build', $.series(clean, $.parallel(styles)));
+$.task('default', $.series('build'));
+// $.task('publish', publish);
 
 function clean() {
   return del(['build']);
 }
 
-function reload(done) {
-  server.reload();
-  done();
-}
+// function reload(done) {
+//   server.reload();
+//   done();
+// }
 
-function watch() {
-  $.watch(['src/index.html', 'src/ref_bib.html'], $.series(pages, reload));
-  $.watch('src/css/*.css', $.series(styles, reload));
-  $.watch(['src/img/*'], $.series(misc, reload));
-}
+// function watch() {
+//   // $.watch(['src/index.html', 'src/ref_bib.html'], $.series(pages, reload));
+//   $.watch('src/css/*.css', $.series(styles, reload));
+//   $.watch(['src/img/*'], $.series(misc, reload));
+// }
 
-function serve(done) {
-  server.init({server: 'build'});
-  done();
-}
+// function serve(done) {
+//   server.init({server: 'build'});
+//   done();
+// }
 
-function pages() {
-  const f = $filter('src/ref_bib.html', {restore: true});
-  return $.src(['src/index.html', 'src/ref_bib.html'])
-    .pipe($changed('build'))
-    .pipe($plumber())
-    .pipe($htmlmin({
-      removeComments: true,
-      collapseWhitespace: true,
-      removeEmptyAttributes: true,
-      minifyJS: true,
-      minifyCSS: true}))
-    .pipe(f)
-    // .pipe($replace('ref.html', 'index.html'))
-    .pipe(f.restore)
-    .pipe($.dest('build'));
-}
+// function pages() {
+//   const f = $filter('src/ref_bib.html', {restore: true});
+//   return $.src(['src/index.html', 'src/ref_bib.html'])
+//     .pipe($changed('build'))
+//     .pipe($plumber())
+//     .pipe($htmlmin({
+//       removeComments: true,
+//       collapseWhitespace: true,
+//       removeEmptyAttributes: true,
+//       minifyJS: true,
+//       minifyCSS: true}))
+//     .pipe(f)
+//     // .pipe($replace('ref.html', 'index.html'))
+//     .pipe(f.restore)
+//     .pipe($.dest('build'));
+// }
 
 function styles() {
   return $.src(['src/css/org-default.css', 'src/css/org.css'])
@@ -64,12 +64,12 @@ function styles() {
     .pipe($.dest('build'));
 }
 
-function misc() {
-  return $.src('src/img/*')
-    .pipe($changed('build'))
-    .pipe($.dest('build/img/'));
-}
+// function misc() {
+//   return $.src('src/img/*')
+//     .pipe($changed('build'))
+//     .pipe($.dest('build/img/'));
+// }
 
-function publish() {
-  return $.src('./build/**/*').pipe($.dest('docs/'));
-}
+// function publish() {
+//   return $.src('./build/**/*').pipe($.dest('docs/'));
+// }
